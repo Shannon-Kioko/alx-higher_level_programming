d8856d79ec3291f5b8b42395195c19d18bf8b3c9
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-06-09 14:28:20 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 import dis
 
+
 def print_names_module():
     # Load the compiled module
     import hidden_4
@@ -8,11 +9,13 @@ def print_names_module():
     bytecode = dis.Bytecode(hidden_4)
 
     # Extract the names of the defined objects
-    names = {instr.argval for instr in bytecode if instr.opname == "LOAD_NAME" and not instr.argval.startswith("__")}
+    names = {instr.argval for instr in bytecode
+             if instr.opname == "LOAD_NAME" and not instr.argval.startswith("__")}
 
     # Print the names in alphabetical order
     for name in sorted(names):
         print(name)
+
 
 if __name__ == "__main__":
     print_names_module()
