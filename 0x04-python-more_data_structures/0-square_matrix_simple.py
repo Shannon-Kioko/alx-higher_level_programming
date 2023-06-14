e0643f7d1b83b287d1863ee7db3c2c97dd6a1d70
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-06-15 01:34:41 +0300

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     Computes square values of all integers of a matrix
     """
     new_m = matrix.copy()
-for i, row in enumerate(new_m):
-    new_m[i] = list(map(lambda x: x**2, row))
+    for i, row in enumerate(new_m):
+        new_m[i] = list(map(lambda x: x**2, row))
     return new_m
