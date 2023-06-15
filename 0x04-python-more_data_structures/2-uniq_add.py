968d101f34b6d2ae1a7743c0b2881a10387de95d
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-06-15 03:53:48 +0300

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,9 +3,3 @@ def uniq_add(my_list=[]):
     my_set = set(my_list)
     res = sum(my_set)
     return res
-
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print(set(my_list))
-
-print("Result: {:d}".format(result))
