1c25915ed76a0cc70d900b51bbb71ae9dc04b36e
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-06-28 01:32:16 +0300

    Create 0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Module 0 - Square class
+"""
+
+class Square:
+	"""
+ 	Represents a square shape.
+	"""
+
+    def __init__(self):
+		"""
+		Initializes a Square instance.
+		"""
+		pass
