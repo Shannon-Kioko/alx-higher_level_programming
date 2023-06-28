ce428281d17a0c50c0f88a750ba23fddc7656d01
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-06-28 13:24:46 +0300

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Module 1: Square
+"""
+
+class Square:
+    """
+    Represents a square shape.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Inits a Square instance with a given size.
+
+        Args:
+            size (int): The size of the square. Must be an integer.
+        """
+        self.__size = size
