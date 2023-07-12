64739c2e10263a31dc587f12364cbf15ef55193e
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-12 19:57:08 +0300

    Add 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+10-square
+"""
+
+
+class Square(Rectangle):
+  def __init__(self, size):
+      """
+      Initializes a Square instance with private attribute size.
+      Validates the size as a positive integer using integer_validator.
+      """
+      self.__size = 0
+      self.integer_validator("size", size)
+      self.__size = size
+      super().__init__(size, size)
+
+  def __str__(self):
+      """
+      Returns a string representation of the Square's dimensions.
+      """
+      return f"[Square] {self.__size}/{self.__size}"
