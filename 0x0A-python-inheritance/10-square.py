509819e6cf47c4573260a5b6dd54ee8f25aae0d8
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-12 20:11:28 +0300

    Refactor code to comply with Pycodestyle spacing conventions

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,18 +6,18 @@
 
 
 class Square(Rectangle):
-  def __init__(self, size):
-      """
-      Initializes a Square instance with private attribute size.
-      Validates the size as a positive integer using integer_validator.
-      """
-      self.__size = 0
-      self.integer_validator("size", size)
-      self.__size = size
-      super().__init__(size, size)
+    def __init__(self, size):
+        """
+        Initializes a Square instance with private attribute size.
+        Validates the size as a positive integer using integer_validator.
+        """
+        self.__size = 0
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
-  def __str__(self):
-      """
-      Returns a string representation of the Square's dimensions.
-      """
-      return f"[Square] {self.__size}/{self.__size}"
+    def __str__(self):
+        """
+        Returns a string representation of the Square's dimensions.
+        """
+        return f"[Square] {self.__size}/{self.__size}"
