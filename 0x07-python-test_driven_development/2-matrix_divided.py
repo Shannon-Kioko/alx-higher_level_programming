f406f4dc9a2222ece309aba6a86fdc2491c7e31b
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-21 17:59:10 +0300

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -31,6 +31,7 @@ def matrix_divided(matrix, div):
     # Check if matrix is a list of lists of integers or floats
     if not all(isinstance(row, list) and
     all(isinstance(elem, (int, float)) for elem in row) for row in matrix):
+
         raise TypeError("matrix must be a matrix (list of lists) \
 of integers/floats")
 
