bc642ef531fcb34b462cfd74eed28cdb747830b2
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-21 15:44:04 +0300

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,12 +3,15 @@
 3-say_my_name module, with tests and documentation
 """
 def say_my_name(first_name, last_name=""):
+
+
     """
     Print the full name in the format 'My name is <first name> <last name>'.
 
     Args:
         first_name (str): The first name of the person.
-        last_name (str, optional): The last name of the person. Defaults to an empty string.
+        last_name (str, optional): The last name of the
+        person. Defaults to an empty string.
 
     Raises:
         TypeError: If first_name or last_name is not a string.
