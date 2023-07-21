b88154f148b0e38ebc207a891dff9be71f737b31
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-21 15:35:31 +0300

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,15 +7,20 @@ on matrices, including division of elements by a given number.
 
 """
 def matrix_divided(matrix, div):
+
+
     """
-    Divide all elements of a matrix by a given number and round to 2 decimal places.
+    Divide all elements of a matrix by a given number
+    and round to 2 decimal places.
 
     Args:
-        matrix (list of lists): A matrix represented as a list of lists of integers or floats.
+        matrix (list of lists): A matrix represented as a
+        list of lists of integers or floats.
         div (int or float): The number to divide each element of the matrix by.
 
     Returns:
-        list of lists: A new matrix where all elements are divided by the given number and rounded to 2 decimal places.
+        list of lists: A new matrix where all elements are divided
+        by the given number and rounded to 2 decimal places.
 
     Raises:
         TypeError: If matrix is not a list of lists of integers or floats.
@@ -24,8 +29,10 @@ def matrix_divided(matrix, div):
         ZeroDivisionError: If div is equal to 0.
     """
     # Check if matrix is a list of lists of integers or floats
-    if not all(isinstance(row, list) and all(isinstance(elem, (int, float)) for elem in row) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(row, list) and all(isinstance(elem, \
+            (int, float)) for elem in row) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) \
+        of integers/floats")
 
     # Check if each row of the matrix has the same size
     row_sizes = set(len(row) for row in matrix)
