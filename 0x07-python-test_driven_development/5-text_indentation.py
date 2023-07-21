30f3430d2fa9be9e1d3672efe23624802e67ff36
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-21 15:14:31 +0300

    Add 5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+def text_indentation(text):
+    """
+    Print a given text with 2 new lines after each '.', '?', and ':' character.
+
+    Args:
+        text (str): The input text.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    punctuation_chars = set('.?:')
+    line = ""
+    for char in text:
+        line += char
+        if char in punctuation_chars:
+            print(line.strip())
+            line = ""
+    if line:
+        print(line.strip())
