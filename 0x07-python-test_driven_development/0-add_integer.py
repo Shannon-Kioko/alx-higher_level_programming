414172b31da6bde76e721befa20e293dfdbb7c01
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-21 15:31:38 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Module on tests with addition"""
 def add_integer(a, b=98):
+
+
     """
     Adds two int integers
 
@@ -25,6 +27,6 @@ def add_integer(a, b=98):
         a = int(a)
 
     if isinstance(b, float):
-        b =int(b)
+        b = int(b)
 
     return a + b
