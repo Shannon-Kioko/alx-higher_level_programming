8f9d3b730bf83ad788be13c6a93502e45c7305c2
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-22 18:25:21 +0300

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -8,6 +8,12 @@
 def add_new_attribute(obj, attribute, value):
     """
     adds a new attribute to an object if it’s possible
+    params:
+        obj: the instance
+        attribute: attribute to add to instance
+        value: the value of newly added instance attribute
+
+    Returns: the new attribute and value
     """
     if not hasattr(obj, "__dict__") and not hasattr(obj, "__slots__"):
         raise TypeError("can't add new attribute")
