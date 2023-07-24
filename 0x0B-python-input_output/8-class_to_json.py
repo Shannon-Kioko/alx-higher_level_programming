12d08c0036b87c9c166e5ea87c4725aa73fbf79c
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-24 21:04:34 +0300

    Add 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+8-class_to_json module
+Contains function that creates a function that returns the dictionary description
+with a simple data structure suitable for JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """
+    Get the dictionary description of an object with simple data structure for JSON serialization.
+
+    Args:
+        obj: An instance of a Class with serializable attributes.
+
+    Returns:
+        dict: A dictionary representation of the object's attributes.
+    """
+    description = {}
+    for attr_name in dir(obj):
+        attr_value = getattr(obj, attr_name)
+        if type(attr_value) in [list, dict, str, int, bool]:
+            description[attr_name] = attr_value
+    return description
