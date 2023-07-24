c670693a3b3e4cf94be0ff0f8b919610c03eff10
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-25 00:29:41 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -17,13 +17,13 @@ def main():
     Adds all arguments to a Python list, and then save them to a file
     """
     if os.path.exists("add_item.json"):
-      my_list = load_from_json_file("add_item.json")
+        my_list = load_from_json_file("add_item.json")
     else:
-      my_list = []
-    
+        my_list = []
+
     # Add command-line arguments to the list
     my_list.extend(sys.argv[1:])
-    
+
     # Save the updated list to "add_item.json"
     save_to_json_file(my_list, "add_item.json")
 
