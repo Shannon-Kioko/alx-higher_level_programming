3f0b1b31c56ac14fc348cb0c7f3b7dab9ebca3eb
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-24 19:59:10 +0300

    Add 4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+4-from_json_string modyle
+Contains functions that returns an object represented by JSON string
+"""
+
+import json
+
+def from_json_string(my_str):
+    """
+    Convert a JSON string to a Python data structure (object).
+
+    Args:
+        my_str (str): The JSON string to be converted.
+
+    Returns:
+        object: The Python data structure represented by the JSON string.
+
+    """
+    return json.loads(my_str)
