5f2c85588c0f9ba4015b46dee9a0d66228dff3e5
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-25 00:28:45 +0300

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -30,16 +30,16 @@ class Student:
         """
         Get the dictionary description of an object with simple data
         structure for JSON serialization.
-        
+
         Args:
         obj: An instance of a Class with serializable attributes.
-        
+
         Returns:
         dict: A dictionary representation of the object's attributes.
         """
         description = {}
         for attr_name in dir(obj):
-        attr_value = getattr(obj, attr_name)
+            attr_value = getattr(obj, attr_name)
         if type(attr_value) in [list, dict, str, int, bool]:
             description[attr_name] = attr_value
         return description
