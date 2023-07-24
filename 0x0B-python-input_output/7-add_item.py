db0cea4178907b6f42e534567c88016d3165abd9
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-25 00:23:16 +0300

    Update 7-add_item.py Pycodestyle conventions

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 7-add_item module
-Contains a function that adds all arguments to a Python list, and then save them to a file
+Contains a function that adds all arguments to a Python list,
+and then save them to a file
 """
 
 import sys
@@ -12,19 +13,20 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 def main():
-  """
-  Adds all arguments to a Python list, and then save them to a file
-  """
+    """
+    Adds all arguments to a Python list, and then save them to a file
+    """
     if os.path.exists("add_item.json"):
-        my_list = load_from_json_file("add_item.json")
+      my_list = load_from_json_file("add_item.json")
     else:
-        my_list = []
-
+      my_list = []
+    
     # Add command-line arguments to the list
     my_list.extend(sys.argv[1:])
-
+    
     # Save the updated list to "add_item.json"
     save_to_json_file(my_list, "add_item.json")
+
 
 if __name__ == "__main__":
     main()
