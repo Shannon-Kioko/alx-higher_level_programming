41848a187c44588cfaea2590462172cf5ae52217
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-25 13:58:05 +0300

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -21,13 +21,14 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self):
         """
         Retrieves a dictionary representation of a Student instance.
 
         Returns:
-            dict: The dictionary representation with a simple data structure for JSON serialization.
+            dict: The dictionary representation with a simple data
+            structure for JSON serialization.
         """
         serializable_attributes = {}
         for key, value in self.__dict__.items():
