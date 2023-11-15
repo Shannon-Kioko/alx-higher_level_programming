81039b14384a394a7cfcf2d071dd47bec3f187bf
Shannon-Kioko <shannonkaluki@gmail.com>
2023-11-15 09:11:43 -0800

    Add semistandard compliance

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
-const Square1 = require("./5-square");
+const Square1 = require('./5-square');
 
 class Square extends Square1 {
-    charPrint(c) {
-        if (!c) {
-            for (let c = 0; c < this.width; c++) {
-                console.log('X'.repeat(this.width));
-            }
-        } else {
-            for (let i = 0; i < this.width; i++) {
-                console.log(c.repeat(this.width));
-            }
-        }
+  charPrint (c) {
+    if (!c) {
+      for (let c = 0; c < this.width; c++) {
+        console.log('X'.repeat(this.width));
+      }
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
+  }
 }
 const s1 = new Square(4);
 s1.charPrint();
 
 s1.charPrint('Z');
-module.exports = Square
+module.exports = Square;
