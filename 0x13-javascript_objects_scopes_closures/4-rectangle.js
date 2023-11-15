50b98512686ac0907451c7886a735e34b8916c67
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-11-15 19:54:24 +0300

    Add 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,44 @@
+#!/usr/bin/node
+class Rectangle {
+    width;
+    height;
+    constructor(w, h) {
+        if (w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+
+    print() {
+        for (let r = 0; r < this.height; r++) {
+            let Recty = ''
+            for (let c = 0; c < this.width; c++) {
+                Recty += "X"
+            }
+                console.log(Recty);
+        }
+    }
+
+    rotate() {
+        [this.width, this.height] = [this.height, this.width]
+    }
+
+    double() {
+        this.width *= 2;
+        this.height *= 2;
+    }
+}
+
+// const r1 = new Rectangle(2, 3);
+// console.log('Normal:');
+// r1.print();
+
+// console.log('Double:');
+// r1.double();
+// r1.print();
+
+// console.log('Rotate:');
+// r1.rotate();
+// r1.print();
+
+module.exports = Rectangle
