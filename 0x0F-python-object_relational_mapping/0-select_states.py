8dc28b4686a8790e5c0392325a128255ca6f8229
Shannon Kioko <shannonkaluki@gmail.com>
2023-11-23 16:45:02 +0300

    Fix requirement issue

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Module that connects to MySQLdb, lists all states from the database hbtn_0e_0_usa
+"""
 import sys
 import MySQLdb
 
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Module lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa
+"""
 from sys import argv
 import MySQLdb
 
