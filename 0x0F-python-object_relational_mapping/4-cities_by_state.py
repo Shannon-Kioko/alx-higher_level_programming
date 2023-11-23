fc044d32456372d02e9faa0fd2d326280aae64e2
Shannon Kioko <shannonkaluki@gmail.com>
2023-11-23 17:28:19 +0300

    Add files

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module akes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+
+    #Connnecting to the db
+    db = MySQLdb.connect(host="localhost", port=3306,user=argv[1], passwd=argv[2], db=argv[3])
+
+    # getting a cursor->gives ability to havem multiple separate working environments through the same
+    # ~connection to the db
+    cur = db.cursor()
+
+    #To execute queries, use the cursor obj and call execute
+    query = "SELECT * FROM cities ORDER BY id ASC"
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
