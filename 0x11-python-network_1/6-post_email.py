227ee2a1004186d81550b49c48f6d95b636a6437
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-12-04 17:34:18 +0300

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-This moduel takes in a URL and an email addy, sends a POST request to the passed
-URL with the email as a parameter, and displays the body of the response.
+This moduel takes in a URL and an email addy, sends a POST request to the
+passed URL with the email as a parameter, and displays
+the body of the response.
 """
 import requests
 import sys
