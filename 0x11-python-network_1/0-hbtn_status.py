45f283936479a6446f91bccf7d5264eb31da1888
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-12-04 17:27:21 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""This module fetches https://alx-intranet.hbtn.io/status"""
+"""
+This module fetches https://alx-intranet.hbtn.io/status using urllib
+"""
 
 if __name__ == "__main__":
     import urllib.request
     import urllib.error
- 
-    url = "https://alx-intranet.hbtn.io/status"
+
     try:
-        with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+        with urllib.request.urlopen(
+            "https://alx-intranet.hbtn.io/status"
+        ) as res:
+            content = res.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(content)))
+            print("\t- content: {}".format(content))
+            print("\t- utf8 content: {}".format(content.decode("utf-8")))
     except urllib.error.URLError as e:
         print(f"Error accessing the URL: {e}")
