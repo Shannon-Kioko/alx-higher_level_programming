084744d0ee5fb40e6d29f6d6b79db00296d1e711
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-12-04 17:16:48 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"This module fetches https://alx-intranet.hbtn.io/status"
+"""This module fetches https://alx-intranet.hbtn.io/status"""
 
 if __name__ == "__main__":
     import urllib.request
